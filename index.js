4f6840c0f36a93dffc909d342eb71877c479f5f2
carly <carly@noukod.com>
2019-03-01 21:16:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-  this.veto = function(){
+  BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
   }
   this.approve = function(){
