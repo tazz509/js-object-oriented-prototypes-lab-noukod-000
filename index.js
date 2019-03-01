c580daf45aa111100a8f595b6baf633bd35a0c19
carly <carly@noukod.com>
2019-03-01 21:16:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function BoardMember(name, homeState, training){
     return "I like to help people."
   }
 
-  this.releasePressStatement = function(){
+    BoardMember.prototype.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
 
